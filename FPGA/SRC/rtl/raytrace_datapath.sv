562450f9dab07c2c3f3d60e265a34d2726081052
Henry Grindheim Hogstad <henrygh90@gmail.com>
2023-10-12 17:13:49 +0200

    Start on datapath

---
+++ FPGA/SRC/rtl/raytrace_datapath.sv
@@ -0,0 +1,13 @@
+`include "types.sv"
+
+module RayTraceDatapath (
+    input logic       clk,
+    input logic[1:0]  index,
+    output logic      valid
+    );
+
+    always @(posedge clk) begin
+        
+    end    
+
+endmodule
