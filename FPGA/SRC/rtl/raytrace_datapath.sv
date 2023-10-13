488317e88424149bcb74e4ee98636bfcd14ec908
Henry Grindheim Hogstad <henrygh90@gmail.com>
2023-10-13 13:58:07 +0200

    FSM for "outer" RayTrace core

--- FPGA/SRC/rtl/raytrace_datapath.sv
+++ FPGA/SRC/rtl/raytrace_datapath.sv
@@ -1,13 +1,42 @@
 `include "types.sv"
 
 module RayTraceDatapath (
-    input logic       clk,
-    input logic[1:0]  index,
-    output logic      valid
+    
+    // Clock
+    input  logic clk,
+    // Internal core control signals    
+    input  logic start,
+    input  logic pixel_done,
+    input  logic next_sphere,
+    // Outwards control signal 
+    output logic busy
+
     );
 
-    always @(posedge clk) begin
-        
-    end    
+    logic next_sphere;
+    logic pixel_done;
 
+    enum int unsigned { START = 0, TRACING = 1 } CURRENT_STATE NEXT_STATE;
+
+    always @(posedge clk) begin
+        case (CURRENT_STATE)
+            START: begin
+                busy <= 0;
+                if(start) begin
+                    NEXT_STATE <= TRACING;
+                end else begin
+                    NEXT_STATE <= START;
+                end
+            end
+
+            TRACING: begin
+               busy <= 1;
+               if(next_sphere) begin
+                   NEXT_STATE <= TRACING;
+               end else if (pixel_done) begin
+                   NEXT_STATE <= START;  
+               end 
+            end
+        endcase
+    end
 endmodule
