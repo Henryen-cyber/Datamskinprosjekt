da23c787ac1edc1a217302d7797fd75125db444c
Henry Grindheim Hogstad <henrygh90@gmail.com>
2023-10-17 14:22:40 +0200

    Extra registers for value-control

--- FPGA/SRC/rtl/raytrace_core.sv
+++ FPGA/SRC/rtl/raytrace_core.sv
@@ -10,16 +10,19 @@ module RayTraceCore(
 
     );
 
-    logic signed[17:0] a_r;
-    logic signed[39:0] c_r;
-    logic signed[20:0] b_r;
+
+    logic signed[50:0] temp_r;
+    logic signed[50:0] a_r;
+    logic signed[50:0] c_r;
+    logic signed[50:0] b_r;
     logic signed[50:0] dis_r;
 
     always @(posedge clk) begin
-        c_r <= world.sphere.radius ** 2 - (world.sphere.origin.x ** 2 + world.sphere.origin.y ** 2 + world.sphere.origin.z ** 2);
-        b_r <= ((pixel.x * world.sphere.origin.x + pixel.y * world.sphere.origin.y + pixel.z * world.sphere.origin.z) << 1);
+        c_r <= (world.sphere.origin.x ** 2 + world.sphere.origin.y ** 2 + world.sphere.origin.z ** 2) - world.sphere.radius ** 2;
+        b_r <= ((pixel.x * world.sphere.origin.x + pixel.y * world.sphere.origin.y + pixel.z * world.sphere.origin.z) * 2);
         a_r <= (pixel.x ** 2 + pixel.y ** 2 + pixel.z ** 2);
-        dis_r <= b_r ** 2 - ((a_r * c_r) << 2);
+        temp_r <= ((a_r * c_r) * 4);
+        dis_r <= b_r ** 2 - temp_r;
         if(dis_r < 0) begin
             less_than_zero <= 1'b1;
         end else begin
